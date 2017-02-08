dc701e56a185c5eda09fff49f93992e5b5f9321e
SierraP <sierrabonet@gmail.com>
2017-02-09 00:13:48 +0100

    Base de datos funcionando

--- src/main/java/participant/MainController.java
+++ src/main/java/participant/MainController.java
@@ -1,6 +1,7 @@
 package participant;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private DBManager database;
 
 	 @RequestMapping("/")
 	    public String landing() {
+		 UserInfo c = database.findByEmail("marcos@gmail.com");
+		 System.out.println(c.getNombre().toString());
 	        return "index";
 	    }
 
--- src/main/java/participant/UserInfo.java
+++ src/main/java/participant/UserInfo.java
@@ -48,6 +48,8 @@ public class UserInfo {
 	private String password;
 
 
+	public UserInfo() {}
+	
 	public UserInfo(long id) {
 		this.id = id;
 	}
