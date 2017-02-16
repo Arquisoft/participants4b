5378d29a8975c0057d6581e7c9c1a279a3c61822
PabloSastre <pablo.bgc@gmail.com>
2017-02-16 20:55:42 +0100

    Password (+psw error) + navegabilidad

--- src/main/java/participant/MainController.java
+++ src/main/java/participant/MainController.java
@@ -1,11 +1,14 @@
 package participant;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,14 +26,22 @@ public class MainController {
 		model.addAttribute("userInfo", new UserInfo());
 		return "index";
 	}
+	
+	
 
 	@RequestMapping("/")
 	public String landingSubmit(@ModelAttribute("userInfo") UserInfo userInfo, Model model) {
 		user = database.findByEmail(userInfo.getEmail());
+		
 		if (user != null) {
 			user.calcularEdad();
 			model.addAttribute("user", user);
-			return "datos";
+			
+			
+			if(user.getPassword().equals(userInfo.getPassword())){
+				return "datos";
+		}
+			return "index_psw";
 		} else
 			return "error";
 	}
